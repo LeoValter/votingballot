79ba11c9f7e0671f4fc40bb55aa1ecc406bd62f9
leovalter83@gmail.com <NqnDffVc4fyq>
2020-01-16 19:45:44 +0300

    migration on HSQLDB

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -3,30 +3,42 @@ DELETE FROM users;
 DELETE FROM restaurants;
 DELETE FROM dishes;
 DELETE FROM votes;
-
-ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE GLOBAL_SEQ RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
 ('Admin', 'admin@gmail.com', '{noop}admin'),
 ('User', 'user@gmail.com', '{noop}password');
 
-INSERT INTO user_roles (user_id, role) VALUES
-(100000, 'ROLE_ADMIN'),
-(100001, 'ROLE_USER');
+
+INSERT INTO user_roles (role, user_id) VALUES
+('ROLE_ADMIN', 100000),
+('ROLE_USER', 100000),
+('ROLE_USER', 100001);
+
 
 INSERT INTO restaurants (name) VALUES
-('Шарманка'),
-('Тарас Бульба'),
-('Караван');
+  ('Шарманка'),
+  ('Тарас Бульба'),
+  ('Караван'),
+  ('8 SECONDS PUB'),
+  ('Ёлки Палки');
 
 INSERT INTO dishes (name, price, restaurant_id, date) VALUES
-('Шашлык', 400, 100002, now()),
-('Борщ', 300, 100003, now()),
-('Плов', 300, 100004, now());
+  ('Шашлык из Каре Ягненка', 450, 100002, '2020-01-16'),
+  ('Домашний суп', 200, 100002, '2020-01-16'),
+  ('Узбекский плов', 330, 100002, '2020-01-16'),
+  ('Фирменный борщ Корчма', 310, 100003, '2020-01-16'),
+  ('Вареники Староукраинские', 310, 100003, '2020-01-16'),
+  ('Шурпа', 485, 100004, '2020-01-16'),
+  ('Самса с тыквой', 145, 100004, '2020-01-16'),
+  ('Салат с осьминогом', 590, 100005, '2020-01-16'),
+  ('Кальмар', 530, 100005, '2020-01-16'),
+  ('Говядина велингтон', 950, 100005, '2020-01-16'),
+  ('Суп из белых грибов', 175, 100006, '2020-01-16'),
+  ('Блины с семгой', 227, 100006, '2020-01-16');
 
-INSERT INTO votes (date, user_id, restaurant_id) VALUES
-(now(), 100000, 100002),
-(now(), 100001, 100003);
 
-
-
+INSERT INTO votes (user_id, restaurant_id, date) VALUES
+  (100000, 100002, '2020-01-15'),
+  (100000, 100005, '2020-01-16'),
+  (100001, 100004, '2020-01-16');
